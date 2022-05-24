cbb61b869bca6e1e51fc6b7a52353196b3307029
sunny-im <hellossun.dahae@gmail.com>
2022-05-24 23:39:12 +0900

    MemberServiceTest

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,8 +9,12 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    // 직접 new 하지 않고 외부에서 넣어줌
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     //회원가입
     public Long join(Member member) {
         //  중복회원 검증
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -11,7 +14,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    // MemberService.java에서도 MemoryMemberRepository 객체 생성했으니까.. 다른 리파지토리를 이용하고 있는거.. 즉 같은 인스턴스를 사용해야되므로
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore(); // 메모리 클리어 !
+    }
 
     @Test
     // 테스트코드니깐 과감하게 한글로 해도 됨 !
@@ -38,7 +53,10 @@ class MemberServiceTest {
         memberService.join(member1);
         // 하단에 try-catch문을 진행하지 않고 하는 방법
         // 람다함수 부분 (memberService.join(member2)가 실행되면 IllegalStateException 예외가 발생)
-        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        // assertThrows은 반환이 된다 !!
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 //        try {
 //            memberService.join(member2);
 //            fail();
